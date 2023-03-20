6606298713ccf8d56e8043db1e3ccfcfe3248827
Jishnu Raj <jishnurajkarockal2001@gmail.com>
2023-03-20 20:04:37 +0530

    doneee

--- index.js
+++ index.js
@@ -13,10 +13,10 @@ setInterval(showNextSentence, 1000);
 
 document.getElementById('resume-link-1').addEventListener("click",()=>{
     // console.log("OPENinig.....")
-    window.location.assign("https://drive.google.com/file/d/1c2IX4RaLn9V_UWALLhcVBCdPFKuII3Cn/view?usp=share_link", "_blank");
+    window.location.open("https://drive.google.com/file/d/1c2IX4RaLn9V_UWALLhcVBCdPFKuII3Cn/view?usp=share_link", "_blank");
   })
   
   document.getElementById('resume-link-2').addEventListener("click",()=>{
     // console.log("OPENinig.....")
-    window.location.assign("https://drive.google.com/file/d/1c2IX4RaLn9V_UWALLhcVBCdPFKuII3Cn/view?usp=share_link", "_blank");
+    window.location.open("https://drive.google.com/file/d/1c2IX4RaLn9V_UWALLhcVBCdPFKuII3Cn/view?usp=share_link", "_blank");
   })
