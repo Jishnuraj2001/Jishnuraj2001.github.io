8967d951f0b272e566583fe2c35334b18bc3e2a5
Jishnu Raj <jishnurajkarockal2001@gmail.com>
2023-03-27 22:49:52 +0530

    done navbar responsiveness

--- index.js
+++ index.js
@@ -9,6 +9,17 @@ function showNextSentence(){
 }
 setInterval(showNextSentence, 1000);
 
+ let sidemenu=document.getElementById("sidemenu");
+
+function openmenu(){
+  console.log(1);
+    sidemenu.style.right="0";
+}
+function closemenu(){
+  console.log(1);
+    sidemenu.style.right="-200px";
+}
+
 
 document.getElementById('resume-link-1').addEventListener("click",()=>{
     window.open("https://drive.google.com/file/d/1c2IX4RaLn9V_UWALLhcVBCdPFKuII3Cn/view?usp=share_link","_blank");
@@ -17,3 +28,7 @@ document.getElementById('resume-link-1').addEventListener("click",()=>{
   document.getElementById('resume-link-2').addEventListener("click",()=>{
     window.open("https://drive.google.com/file/d/1c2IX4RaLn9V_UWALLhcVBCdPFKuII3Cn/view?usp=share_link","_blank");
   })
+
+
+
+
